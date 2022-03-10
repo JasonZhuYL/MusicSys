a5b105b646fd52295daeb680444b5c68f59d4d3d
JasonZhuYL <zhuyuliang0817@gmail.com>
2022-03-10 23:10:20 +0000

    added qingtian Ver1

--- ZYL_part1.cpp
+++ ZYL_part1.cpp
@@ -6,6 +6,8 @@
 const uint32_t interval = 100; // Display update interval
 volatile int32_t currentStepSize;
 volatile String currentNote;
+volatile uint8_t noteIndex;
+volatile uint8_t volume; // range from 0 to 16
 const int32_t stepSizes[] = {262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 0};
 const String note[] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", ""};
 
@@ -37,235 +39,348 @@ const int DRST_BIT = 4;
 const int HKOW_BIT = 5;
 const int HKOE_BIT = 6;
 
-static int32_t phaseAcc = 0;
-volatile uint8_t keyArray[7];
-
 // Display driver object
 U8G2_SSD1305_128X32_NONAME_F_HW_I2C u8g2(U8G2_R0);
+
+static int32_t phaseAcc = 0;
+volatile uint8_t keyArray[7];
+SemaphoreHandle_t keyArrayMutex;
 
 // Function to set outputs using key matrix
 void setOutMuxBit(const uint8_t bitIdx, const bool value)
 {
-  digitalWrite(REN_PIN, LOW);
-  digitalWrite(RA0_PIN, bitIdx & 0x01);
-  digitalWrite(RA1_PIN, bitIdx & 0x02);
-  digitalWrite(RA2_PIN, bitIdx & 0x04);
-  digitalWrite(OUT_PIN, value);
-  digitalWrite(REN_PIN, HIGH);
-  delayMicroseconds(2);
-  digitalWrite(REN_PIN, LOW);
+    digitalWrite(REN_PIN, LOW);
+    digitalWrite(RA0_PIN, bitIdx & 0x01);
+    digitalWrite(RA1_PIN, bitIdx & 0x02);
+    digitalWrite(RA2_PIN, bitIdx & 0x04);
+    digitalWrite(OUT_PIN, value);
+    digitalWrite(REN_PIN, HIGH);
+    delayMicroseconds(2);
+    digitalWrite(REN_PIN, LOW);
 }
 
 uint8_t readCols()
 {
-  uint8_t C0 = digitalRead(C0_PIN);
-  uint8_t C1 = digitalRead(C1_PIN);
-  uint8_t C2 = digitalRead(C2_PIN);
-  uint8_t C3 = digitalRead(C3_PIN);
-  uint8_t byteRead = (C3 << 3) | (C2 << 2) | (C1 << 1) | C0;
-  return byteRead;
+    uint8_t C0 = digitalRead(C0_PIN);
+    uint8_t C1 = digitalRead(C1_PIN);
+    uint8_t C2 = digitalRead(C2_PIN);
+    uint8_t C3 = digitalRead(C3_PIN);
+    uint8_t byteRead = (C3 << 3) | (C2 << 2) | (C1 << 1) | C0;
+    return byteRead;
 }
 
 void setRow(uint8_t rowIdx)
 {
 
-  digitalWrite(REN_PIN, LOW);
-  if (bitRead(rowIdx, 0) == 0)
-  {
-    digitalWrite(RA0_PIN, LOW);
-  }
-  else
-  {
-    digitalWrite(RA0_PIN, HIGH);
-  }
-  if (bitRead(rowIdx, 1) == 0)
-  {
-    digitalWrite(RA1_PIN, LOW);
-  }
-  else
-  {
-    digitalWrite(RA1_PIN, HIGH);
-  }
-  if (bitRead(rowIdx, 2) == 0)
-  {
-    digitalWrite(RA2_PIN, LOW);
-  }
-  else
-  {
-    digitalWrite(RA2_PIN, HIGH);
-  }
-  digitalWrite(REN_PIN, HIGH);
+    digitalWrite(REN_PIN, LOW);
+    if (bitRead(rowIdx, 0) == 0)
+    {
+        digitalWrite(RA0_PIN, LOW);
+    }
+    else
+    {
+        digitalWrite(RA0_PIN, HIGH);
+    }
+    if (bitRead(rowIdx, 1) == 0)
+    {
+        digitalWrite(RA1_PIN, LOW);
+    }
+    else
+    {
+        digitalWrite(RA1_PIN, HIGH);
+    }
+    if (bitRead(rowIdx, 2) == 0)
+    {
+        digitalWrite(RA2_PIN, LOW);
+    }
+    else
+    {
+        digitalWrite(RA2_PIN, HIGH);
+    }
+    digitalWrite(REN_PIN, HIGH);
 }
 
 void sampleISR()
 {
-  phaseAcc += currentStepSize * 195225;
-  int32_t Vout = phaseAcc >> 24;
-  analogWrite(OUTR_PIN, Vout + 128);
+    phaseAcc += currentStepSize * 195225;
+    int32_t Vout = phaseAcc >> 24;
+    Vout = Vout >> (8 - int((volume + 4) * 0.8) / 2);
+    analogWrite(OUTR_PIN, Vout + 128);
 }
 
-void readStep(volatile uint8_t keyArray[7])
+void readStep(volatile uint8_t keyArrayL[7])
 {
-  // uint32_t oldKey = keyNum;
-  // find the corresponding stepsize
-  uint32_t keyNum = 12;
+    // uint32_t oldKey = keyNum;
+    // find the corresponding stepsize
+    uint32_t keyNum = 12;
 
-  if (bitRead(keyArray[0], 0) == 0)
-  {
-    keyNum = 0;
-  }
-  else if (bitRead(keyArray[0], 1) == 0)
-  {
-    keyNum = 1;
-  }
-  else if (bitRead(keyArray[0], 2) == 0)
-  {
-    keyNum = 2;
-  }
-  else if (bitRead(keyArray[0], 3) == 0)
-  {
-    keyNum = 3;
-  }
-  else if (bitRead(keyArray[1], 0) == 0)
-  {
-    keyNum = 4;
-  }
-  else if (bitRead(keyArray[1], 1) == 0)
-  {
-    keyNum = 5;
-  }
-  else if (bitRead(keyArray[1], 2) == 0)
-  {
-    keyNum = 6;
-  }
-  else if (bitRead(keyArray[1], 3) == 0)
-  {
-    keyNum = 7;
-  }
-  else if (bitRead(keyArray[2], 0) == 0)
-  {
-    keyNum = 8;
-  }
-  else if (bitRead(keyArray[2], 1) == 0)
-  {
-    keyNum = 9;
-  }
-  else if (bitRead(keyArray[2], 2) == 0)
-  {
-    keyNum = 10;
-  }
-  else if (bitRead(keyArray[2], 3) == 0)
-  {
-    keyNum = 11;
-  }
-  else
-  {
-    keyNum = 12;
-  }
-  // int keyDiff = keyNum - oldKey;
-  __atomic_store_n(&currentStepSize, stepSizes[keyNum], __ATOMIC_RELAXED);
+    if (bitRead(keyArrayL[0], 0) == 0)
+    {
+        keyNum = 0;
+    }
+    else if (bitRead(keyArrayL[0], 1) == 0)
+    {
+        keyNum = 1;
+    }
+    else if (bitRead(keyArrayL[0], 2) == 0)
+    {
+        keyNum = 2;
+    }
+    else if (bitRead(keyArrayL[0], 3) == 0)
+    {
+        keyNum = 3;
+    }
+    else if (bitRead(keyArrayL[1], 0) == 0)
+    {
+        keyNum = 4;
+    }
+    else if (bitRead(keyArrayL[1], 1) == 0)
+    {
+        keyNum = 5;
+    }
+    else if (bitRead(keyArrayL[1], 2) == 0)
+    {
+        keyNum = 6;
+    }
+    else if (bitRead(keyArrayL[1], 3) == 0)
+    {
+        keyNum = 7;
+    }
+    else if (bitRead(keyArrayL[2], 0) == 0)
+    {
+        keyNum = 8;
+    }
+    else if (bitRead(keyArrayL[2], 1) == 0)
+    {
+        keyNum = 9;
+    }
+    else if (bitRead(keyArrayL[2], 2) == 0)
+    {
+        keyNum = 10;
+    }
+    else if (bitRead(keyArrayL[2], 3) == 0)
+    {
+        keyNum = 11;
+    }
+    else
+    {
+        keyNum = 12;
+    }
+    noteIndex = keyNum;
+    __atomic_store_n(&currentStepSize, stepSizes[keyNum], __ATOMIC_RELAXED);
+}
 
-  // if (keyDiff > 0)
+int knobDecode(volatile uint8_t previous, volatile uint8_t current)
+{
+    // only two bits are used in each param
+    if (previous > 1)
+    {
+        previous = ~(previous | 0xFC);
+        current = ~(current | 0xFC);
+    }
+    switch (previous)
+    {
+    case 0:
+        switch (current)
+        {
+        case 1:
+            return 1;
+        case 2:
+            return -1;
+        case 3:
+            return 0;
+        default:
+            return 0;
+        }
+    case 1:
+        switch (current)
+        {
+        case 3:
+            return 1;
+        case 0:
+            return -1;
+        case 2:
+            return 0;
+        default:
+            return 0;
+        }
+    }
+    return 0;
+}
 
-  // currentStepSize = stepSizes[keyNum];
+int noteDemux(int note){
+    if(note ==0){
+        return 12;
+    }
+    if(note>3){
+        return (note*2)-3;
+    }else{
+        return (note*2)-1;
+    }
+}
+
+void play_Twinkle_star()
+{
+    int twinkleStar[] = {1,1,8,8,10,10,8, 6,6,5,5,3,3,1  ,8,8,6,6,5,5,3, 8,8,6,6,5,5,3, 1,1,8,8,10,10,8, 6,6,5,5,3,3,1};
+    
+    for (int i = 0; i < 42; i++)
+    {
+        if ((i + 1) % 7 == 0)
+        {
+            __atomic_store_n(&currentStepSize, stepSizes[twinkleStar[i]-1], __ATOMIC_RELAXED);
+            delayMicroseconds(200000);
+            __atomic_store_n(&currentStepSize, 0, __ATOMIC_RELAXED);
+            delayMicroseconds(150000);
+        }
+        else
+        {
+            __atomic_store_n(&currentStepSize, stepSizes[twinkleStar[i]-1], __ATOMIC_RELAXED);
+            delayMicroseconds(200000);
+            __atomic_store_n(&currentStepSize, 0, __ATOMIC_RELAXED);
+            delayMicroseconds(100000);
+        }
+    }
+}
+
+void play_QinTian(){
+    uint8_t qts[] = {0,5,5,1,1,2,3,0,5,5,1,1,2,3,2,1,5  ,0,5,5,1,1,2,3,0,3,2,3,4,3,2,4,3,2,1};
+    uint8_t oct[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+    uint8_t del[] = {2,2,2,2,4,2,2,2,2,2,2,2,1,1,1,1,2,2,2,2,2,4,2,2,2,4,1,1,1,1,1,1,1,1,1};
+    int current = 0;
+    int len = sizeof(qts)/sizeof(*qts);
+    for(int i=0;i<len-1;i++){
+        if(oct[i]==2){
+            current = stepSizes[noteDemux(qts[i])]/2;
+            }else{
+            current = stepSizes[noteDemux(qts[i])];}
+        __atomic_store_n(&currentStepSize, current, __ATOMIC_RELAXED);
+        delayMicroseconds(del[i]*100000);
+        __atomic_store_n(&currentStepSize, 0, __ATOMIC_RELAXED);
+        delayMicroseconds(50000);
+    }
+    Serial.println("finished");
 }
 
 void scanKeysTask(void *pvParameters)
 {
-  const TickType_t xFrequency = 50 / portTICK_PERIOD_MS;
-  TickType_t xLastWakeTime = xTaskGetTickCount();
-  while (1)
-  {
-    vTaskDelayUntil(&xLastWakeTime, xFrequency);
-    for (uint8_t i = 0; i < 3; i++)
+    const TickType_t xFrequency = 50 / portTICK_PERIOD_MS;
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+    volatile uint8_t knobState = 0;
+    volatile uint8_t knob3_stat = 0;
+    volatile uint8_t knob3_stat_pre = 1;
+    while (1)
     {
-      setRow(i);
-      delayMicroseconds(3);
-      keyArray[i] = readCols();
+        vTaskDelayUntil(&xLastWakeTime, xFrequency);
+        for (uint8_t i = 0; i < 5; i++)
+        {
+            setRow(i);
+            delayMicroseconds(3);
+            // xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
+            keyArray[i] = readCols();
+            // xSemaphoreGive(keyArrayMutex);
+        }
+        // xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
+        readStep(keyArray);
+
+        knob3_stat = knobDecode((knobState & 0b11000000) >> 6, keyArray[3] & 0b00000011);
+        knobState = ((keyArray[3] & 0b00000011) << 6) | (knobState & 0b00111111);
+        volume += knob3_stat;
+        if (volume > 16)
+        {
+            volume = 16;
+            play_QinTian();
+            Serial.println("QINGTIAN now playing");
+        }
     }
-    readStep(keyArray);
-  }
 }
 
 void displayUpdateTask(void *pvParameters)
 {
-  const TickType_t xFrequency = 100 / portTICK_PERIOD_MS;
-  TickType_t xLastWakeTime = xTaskGetTickCount();
-  while (1)
-  {
-    vTaskDelayUntil(&xLastWakeTime, xFrequency);
-    u8g2.clearBuffer();                  // clear the internal memory
-    u8g2.setFont(u8g2_font_ncenB08_tr);  // choose a suitable font
-    u8g2.drawStr(2, 10, "Hello World!"); // write something to the internal memory
-    for (uint8_t i = 0; i < 3; i++)
+    const TickType_t xFrequency = 100 / portTICK_PERIOD_MS;
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+
+    while (1)
     {
-      u8g2.print(keyArray[i], HEX);
+        vTaskDelayUntil(&xLastWakeTime, xFrequency);
+        u8g2.clearBuffer();                 // clear the internal memory
+        u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
+        u8g2.drawStr(2, 10, "Hola!");       // write something to the internal memory
+        for (uint8_t i = 0; i < 3; i++)
+        {
+            // xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
+            u8g2.print(keyArray[i], HEX);
+            // xSemaphoreGive(keyArrayMutex);
+        }
+        u8g2.drawStr(2, 30, (note[noteIndex]).c_str()); // write something to the internal memory
+        u8g2.print(volume, DEC);
+        u8g2.print(currentStepSize, DEC);
+        u8g2.setCursor(2, 20);
+        u8g2.sendBuffer(); // transfer internal memory to the display
     }
-    u8g2.print(currentStepSize, DEC);
-    u8g2.setCursor(2, 20);
-    u8g2.sendBuffer(); // transfer internal memory to the display
-  }
 }
 
 void setup()
 {
-  // put your setup code here, to run once:
-  // Set pin directions
-  pinMode(RA0_PIN, OUTPUT);
-  pinMode(RA1_PIN, OUTPUT);
-  pinMode(RA2_PIN, OUTPUT);
-  pinMode(REN_PIN, OUTPUT);
-  pinMode(OUT_PIN, OUTPUT);
-  pinMode(OUTL_PIN, OUTPUT);
-  pinMode(OUTR_PIN, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+    // put your setup code here, to run once:
+    // Set pin directions
+    pinMode(RA0_PIN, OUTPUT);
+    pinMode(RA1_PIN, OUTPUT);
+    pinMode(RA2_PIN, OUTPUT);
+    pinMode(REN_PIN, OUTPUT);
+    pinMode(OUT_PIN, OUTPUT);
+    pinMode(OUTL_PIN, OUTPUT);
+    pinMode(OUTR_PIN, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 
-  pinMode(C0_PIN, INPUT);
-  pinMode(C1_PIN, INPUT);
-  pinMode(C2_PIN, INPUT);
-  pinMode(C3_PIN, INPUT);
-  pinMode(JOYX_PIN, INPUT);
-  pinMode(JOYY_PIN, INPUT);
+    pinMode(C0_PIN, INPUT);
+    pinMode(C1_PIN, INPUT);
+    pinMode(C2_PIN, INPUT);
+    pinMode(C3_PIN, INPUT);
+    pinMode(JOYX_PIN, INPUT);
+    pinMode(JOYY_PIN, INPUT);
 
-  // Initialise display
-  setOutMuxBit(DRST_BIT, LOW); // Assert display logic reset
-  delayMicroseconds(2);
-  setOutMuxBit(DRST_BIT, HIGH); // Release display logic reset
-  u8g2.begin();
-  setOutMuxBit(DEN_BIT, HIGH); // Enable display power supply
+    // Initialise display
+    setOutMuxBit(DRST_BIT, LOW); // Assert display logic reset
+    delayMicroseconds(2);
+    setOutMuxBit(DRST_BIT, HIGH); // Release display logic reset
+    u8g2.begin();
+    setOutMuxBit(DEN_BIT, HIGH); // Enable display power supply
 
-  TIM_TypeDef *Instance = TIM1;
-  HardwareTimer *sampleTimer = new HardwareTimer(Instance);
-  sampleTimer->setOverflow(22000, HERTZ_FORMAT);
-  sampleTimer->attachInterrupt(sampleISR);
-  sampleTimer->resume();
+    TIM_TypeDef *Instance = TIM1;
+    HardwareTimer *sampleTimer = new HardwareTimer(Instance);
+    sampleTimer->setOverflow(22000, HERTZ_FORMAT);
+    sampleTimer->attachInterrupt(sampleISR);
+    sampleTimer->resume();
 
-  // Initialise UART
-  Serial.begin(9600);
-  Serial.println("Hello World");
+    // Initialise UART
+    Serial.begin(9600);
+    Serial.println("Hello World");
 
-  // RTOS initialize thread
-  TaskHandle_t scanKeysHandle = NULL;
-  xTaskCreate(
-      scanKeysTask,   /* Function that implements the task */
-      "scanKeys",     /* Text name for the task */
-      64,             /* Stack size in words, not bytes */
-      NULL,           /* Parameter passed into the task */
-      1,              /* Task priority */
-      &scanKeysHandle /* Pointer to store the task handle */
-  );
+    // RTOS initialize thread
+    TaskHandle_t scanKeysHandle = NULL;
+    xTaskCreate(
+        scanKeysTask,   /* Function that implements the task */
+        "scanKeys",     /* Text name for the task */
+        64,             /* Stack size in words, not bytes */
+        NULL,           /* Parameter passed into the task */
+        1,              /* Task priority */
+        &scanKeysHandle /* Pointer to store the task handle */
+    );
+    TaskHandle_t displayUpdateHandle = NULL;
+    xTaskCreate(
+        displayUpdateTask,
+        "displayUpdate",
+        256,
+        NULL,
+        2,
+        &displayUpdateHandle);
 
-  TaskHandle_t displayUpdateHandle = NULL;
-  xTaskCreate(
-      displayUpdateTask,
-      "displayUpdate",
-      256,
-      NULL,
-      2,
-      &displayUpdateHandle);
+    // Start of RTOS scheduler
+    vTaskStartScheduler();
+    keyArrayMutex = xSemaphoreCreateMutex();
 
-  // Start of RTOS scheduler
-  vTaskStartScheduler();
 }
 
 void loop() {}
+
+
