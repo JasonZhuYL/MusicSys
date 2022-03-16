d25ce596ae600ed542ec2fcd442597e60fdee88d
JasonZhuYL <zhuyuliang0817@gmail.com>
2022-03-16 11:21:26 +0000

    knob class

--- knob.h
+++ knob.h
@@ -1,66 +1,78 @@
 #ifndef KNOB_H
 #define KNOB_H
 
-class knob_decoder{
-    private:
-        volatile uint8_t previous
-        volatile uint8_t rotationValue
-    public:
-        int knobDecode(volatile uint8_t current)
+class knob_decoder
+{
+private:
+    uint8_t rotationValue;
+    uint8_t previous;
+    uint8_t upper_limit;
+    uint8_t lower_limit;
+
+public:
+    knob_decoder(uint8_t initial_Val, uint8_t upper, uint8_t lower)
+        : rotationValue(initial_Val), upper_limit(upper), lower_limit(lower)
+    {
+    }
+    ~knob_decoder();
+    void update(const uint8_t current);
+    uint8_t get_val();
+    void change_val(const uint8_t new_Val);
+};
+
+uint8_t knob_decoder::get_val()
+{
+    return rotationValue;
 }
 
-
-volatile uint8_t knobDecode(volatile uint8_t previous, volatile uint8_t current)
+void knob_decoder::change_val(const uint8_t new_Val)
 {
-    // only two bits are used in each param
-    if (previous > 1)
+    self.roationValue = new_Val;
+}
+
+void knob_decoder::update(const uint8_t current)
+{
+    // The format of current should be 0b00000011
+    // The previous would always be    0b00000011
+    uint8_t standardised_current = current;
+    if (self.previous > 1)
     {
-        previous = ~(previous | 0xFC);
-        current = ~(current | 0xFC);
+        self.previous = ~(self.previous | 0xFC);
+        standardised_current = ~(current | 0xFC);
     }
-    switch (previous)
+
+    switch (self.previous)
     {
     case 0:
-        switch (current)
+        switch (standardised_current)
         {
         case 1:
-            return 1;
+            rotationValue += 1;
         case 2:
-            return -1;
+            rotationValue -= 1;
         case 3:
-            return 0;
         default:
-            return 0;
         }
     case 1:
-        switch (current)
+        switch (standardised_current)
         {
         case 3:
-            return 1;
+            rotationValue += 1;
         case 0:
-            return -1;
+            rotationValue -= 1;
         case 2:
-            return 0;
         default:
-            return 0;
         }
     }
-    return 0;
-
-volatile uint8_t knobState(volatile uint8_t keyArray, int knobNumber){
-    switch (knobNumber)
+    if (rotationValue > upper_limit)
     {
-    case 0:
-        return ((keyArray & 0b00001100)) >> 2 | (knobState & 0b11111100)
-        break;
-    
-    default:
-        break;
+        rotationValue = upper_limit;
     }
+    // Currently using the uint8_t thus 255+1 = 0
+    //  Lower limit function could not be implemented.
+    //  if (rotationValue  upper_limit){
+    //      rotationValue = upper_limit;
+    //  }
 }
-
-}
-
-
 
 #endif
